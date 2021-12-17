e81fff9160a42cc22c9fe081c649f46680e23ada
Zakaria-Shahen <zikoshahen1@gmial.com>
2021-12-17 14:50:45 +0200

    Done unit tests: checkForName()

--- __test__/nameChecker.test.js
+++ __test__/nameChecker.test.js
@@ -1,16 +1,29 @@
 const nameChecker = require('../src/client/js/nameChecker')
 
-jest.mock("window", 'alert').mockImplementation(() => true)
+global.alert = jest.fn(value => true)
+global.console.log = jest.fn()
+beforeEach(() => {
+    alert.mockClear()
+    console.log.mockClear()
+})
 
 
-describe("testing formHandler", () => {
+describe("checkForName(): ", () => {
+
     const input = ["Picard", "Janeway", "Kirk", "Archer", "Georgiou"]
-    test.each(input)("test name output", (a) => {
-        
-        expect(window.alert).toBeCalledWith(true)
-        expect(nameChecker(a))
+    const inputWrong = ["zakaria", "omar", "github", "FWD"]
+
+    it.each(input)("using alert/console.log arg %j", (a) => {
+        expect(nameChecker.checkForName(a)).toBeUndefined()
+        expect(alert).toBeCalledTimes(1)
+
     })
-    
+
+    it.each(inputWrong)("not using alert/console.log arg %j", a => {
+        expect(nameChecker.checkForName(a)).toBeUndefined()
+        expect(alert).toBeCalledTimes(0)
+        expect(console.log).toBeCalledTimes(1)
+    })
 
 
 })
